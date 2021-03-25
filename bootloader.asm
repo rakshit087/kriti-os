2320da98407697bcf68321877be6bcf374c9a5d2
Rakshit Arora <rakshit087@gmail.com>
2021-03-25 14:28:32 +0530

    Switched to 32 bits

--- bootloader.asm
+++ bootloader.asm
@@ -1,9 +1,25 @@
-; Infinite loop
-loop:
-    jmp loop 
+[org 0x7c00]
 
-; Fill with 510 zeros minus the size of the previous code (since bootloader is 512 bytes)
+mov bp , 0x9000 ;stack
+mov sp , bp
 
-times 510-($-$$) db 0
-; Magic number (2 bytes) It tells the computer about bootloader 
-dw 0xaa55 
+call switch_to_pm 
+
+jmp $
+%include "gdt.asm"
+%include "print.asm"
+%include "enter32bit.asm"
+
+[ bits 32]
+BEGIN_PM :
+    mov ebx , PRINT_ME
+    call print 
+jmp $
+
+
+PRINT_ME db "Running on 32 bits " , 0
+
+;filling rest of the code with 0s
+times 510 -( $ - $$ ) db 0
+;magic number
+dw 0xaa55
---
+++ gdt.asm
@@ -0,0 +1,33 @@
+gdt_start :
+
+; null descripter
+gdt_null : 
+dd 0x0 
+dd 0x0
+
+;code segment descriptor
+gdt_code : 
+dw 0xffff
+dw 0x0 
+db 0x0 
+db 10011010b ; 1st flags
+db 11001111b ; 2nd flags
+db 0x0
+
+;data segment descriptor
+gdt_data :
+dw 0xffff
+dw 0x0 
+db 0x0 
+db 10010010b ; 1st flags
+db 11001111b ; 2nd flags
+db 0x0 
+
+gdt_end :
+
+gdt_descriptor :
+; Size of our GDT 
+dw gdt_end - gdt_start - 1 
+dd gdt_start 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
